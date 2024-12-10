611061a076dfcbaaf94f119da15792140f836a8f
CodeSandbox <bosuutap@alwaysdata.net>
2024-12-10 08:12:02 +0000

    ...

--- libs/s3Client.js
+++ libs/s3Client.js
@@ -2,11 +2,10 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3Client = new S3Client({
-  endpoint: process.env.ENDPOINT || "https://q0w7.sg.idrivee2-43.com",
+  endpoint: "https://q0w7.sg.idrivee2-43.com",
   credentials: {
-    accessKeyId: process.env.KEY || "8OBl9ve6KBiLAdnIReel",
-    secretAccessKey:
-      process.env.SECRET || "RwoILuzZVKcRhtFfvXFSZqf6vHkPeF5eeWYvviy5",
+    accessKeyId: "8OBl9ve6KBiLAdnIReel",
+    secretAccessKey: "RwoILuzZVKcRhtFfvXFSZqf6vHkPeF5eeWYvviy5",
   },
   forcePathStyle: true,
   region: "ap-southeast-1",
